749271327298a3693b4d095d30d00ead8b369f28
lovinskr <55713318+lovinskr@users.noreply.github.com>
2021-07-22 15:31:14 -0400

    Net Ninja Tutorial #6 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="content">
+        <Home/> 
+      </div>
     </div>
   );
 }
